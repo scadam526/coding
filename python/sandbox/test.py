66b03820c62f382bd2ae866d6452c084cd9fad06
Shawn <shawnadams@gmail.com>
2023-12-01 13:53:56 -0500

    prompt for and parse equation, extract and symbols, prompt for solve-for symbol, print all solutions

--- python/sandbox/test.py
+++ python/sandbox/test.py
@@ -1,8 +1,10 @@
-from sympy import sympify, solve, Eq
+from sympy import sympify, solve, Eq, pprint
 
-# Example equation string
-# equation_string = "Vfb = Vs * (Rb / (Rb + Rt))"
-# equation_string = "y = a*x**2+b*x+c"
+# Example equations
+# Vfb = Vs * (Rb / (Rb + Rt))
+# y = a*x**2+b*x+c
+
+# prompt for equation
 equation_string = input("Enter equation (<left side = right side>) : ")
 
 # Split into left and right side and sympify
@@ -15,10 +17,14 @@ expression = Eq(sympify(leftTxt), sympify(rightTxt))
 # Collect list of symbols from left and right side
 all_symbols = sympify(rightTxt).free_symbols
 all_symbols = all_symbols.union(sympify(leftTxt).free_symbols)
-declared_symbols = list(all_symbols)
-print(f"Symbols: {declared_symbols}\n", type(sympify('x')))
 
+# turn symbols into a list
+declared_symbols = list(all_symbols)
+
+# print available symbols and prompt for solve-for symbol
+print(f"Symbols: {declared_symbols}\n")
 solve_sym = sympify(input(f"Enter solve-for symbol: "))
+
+# solve and print
 solution = solve(expression, solve_sym)
-for i in solution:
-    print(f"{solve_sym} = {i}")
+pprint(solution)
